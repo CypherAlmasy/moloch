50d0a6a4cd1f9cd30220731c42a4f1f5ff583946
Matthew Weir <mcweir@ADT.COM>
2018-03-27 14:34:49 -0700

    adding apt-get update

--- easybutton-build.sh
+++ easybutton-build.sh
@@ -77,6 +77,7 @@ if [ -f "/etc/redhat-release" ]; then
 fi
 
 if [ -f "/etc/debian_version" ]; then
+  apt-get update
   apt-get -y install wget curl libpcre3-dev uuid-dev libmagic-dev pkg-config g++ flex bison zlib1g-dev libffi-dev gettext libgeoip-dev make libjson-perl libbz2-dev libwww-perl libpng-dev xz-utils libffi-dev libssl-dev libreadline-dev libtool libyaml-dev dh-autoreconf
   if [ $? -ne 0 ]; then
     echo "MOLOCH: apt-get failed"
